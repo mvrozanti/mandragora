7f5586d8aad93cb49b7bb53b457b9960ab84eb8f
Marcelo <mvrozanti@hotmail.com>
2019-07-09 00:10:53 -0300

    cfr+=_

--- .config/ranger/commands.py
+++ .config/ranger/commands.py
@@ -90,6 +90,16 @@ class fasd(Command):
             directory = subprocess.check_output(["fasd", "-d"]+arg.split(), universal_newlines=True).strip()
             self.fm.cd(directory)
 
+class tmsu_tag(Command):
+    """:tmsu_tag
+
+    Tags the current file with tmsu
+    """
+
+    def execute(self):
+        cf = self.fm.thisfile
+
+        self.fm.run("tmsu tag \"{0}\" {1}".format(cf.basename, self.rest(1)))
 
 class exif_filter(Command):
     """
