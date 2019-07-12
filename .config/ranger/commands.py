65feac13a81be1ee3260f5e9a4ceab33841be468
Marcelo <mvrozanti@hotmail.com>
2019-07-12 13:34:58 -0300

    cfr≃gs

--- .config/ranger/commands.py
+++ .config/ranger/commands.py
@@ -4,6 +4,125 @@ from collections import deque
 import os
 fd_deq = deque()
 
+class ag(Command):
+    import re
+    """:ag 'regex'
+    Looks for a string in all marked paths or current dir
+    """
+    editor = os.getenv('EDITOR') or 'vim'
+    acmd = 'ag --smart-case --group --color --hidden'  # --search-zip
+    qarg = re.compile(r"""^(".*"|'.*')$""")
+    patterns = []
+    # THINK:USE: set_clipboard on each direct ':ag' search? So I could find in vim easily
+
+    def _sel(self):
+        d = self.fm.thisdir
+        if d.marked_items:
+            return [f.relative_path for f in d.marked_items]
+        # WARN: permanently hidden files like .* are searched anyways
+        #   << BUG: files skipped in .agignore are grep'ed being added on cmdline
+        if d.temporary_filter and d.files_all and (len(d.files_all) != len(d.files)):
+            return [f.relative_path for f in d.files]
+        return []
+
+    def _arg(self, i=1):
+        if self.rest(i):
+            ag.patterns.append(self.rest(i))
+        return ag.patterns[-1] if ag.patterns else ''
+
+    def _quot(self, patt):
+        return patt if ag.qarg.match(patt) else shell_quote(patt)
+
+    def _bare(self, patt):
+        return patt[1:-1] if ag.qarg.match(patt) else patt
+
+    def _aug_vim(self, iarg, comm='Ag'):
+        if self.arg(iarg) == '-Q':
+            self.shift()
+            comm = 'sil AgSet def.e.literal 1|' + comm
+        # patt = self._quot(self._arg(iarg))
+        patt = self._arg(iarg)  # No need to quote in new ag.vim
+        # FIXME:(add support)  'AgPaths' + self._sel()
+        cmd = ' '.join([comm, patt])
+        cmdl = [ag.editor, '-c', cmd, '-c', 'only']
+        return (cmdl, '')
+
+    def _aug_sh(self, iarg, flags=[]):
+        cmdl = ag.acmd.split() + flags
+        if iarg == 1:
+            import shlex
+            cmdl += shlex.split(self.rest(iarg))
+        else:
+            # NOTE: only allowed switches
+            opt = self.arg(iarg)
+            while opt in ['-Q', '-w']:
+                self.shift()
+                cmdl.append(opt)
+                opt = self.arg(iarg)
+            # TODO: save -Q/-w into ag.patterns =NEED rewrite plugin to join _aug*()
+            patt = self._bare(self._arg(iarg))  # THINK? use shlex.split() also/instead
+            cmdl.append(patt)
+        if '-g' not in flags:
+            cmdl += self._sel()
+        return (cmdl, '-p')
+
+    def _choose(self):
+        if self.arg(1) == '-v':
+            return self._aug_vim(2, 'Ag')
+        elif self.arg(1) == '-g':
+            return self._aug_vim(2, 'sil AgView grp|Ag')
+        elif self.arg(1) == '-l':
+            return self._aug_sh(2, ['--files-with-matches', '--count'])
+        elif self.arg(1) == '-p':  # paths
+            return self._aug_sh(2, ['-g'])
+        elif self.arg(1) == '-f':
+            return self._aug_sh(2)
+        elif self.arg(1) == '-r':
+            return self._aug_sh(2, ['--files-with-matches'])
+        else:
+            return self._aug_sh(1)
+
+    def _catch(self, cmd):
+        from subprocess import check_output, CalledProcessError
+        try:
+            out = check_output(cmd)
+        except CalledProcessError:
+            return None
+        else:
+            return out[:-1].decode('utf-8').splitlines()
+
+    # DEV
+    # NOTE: regex becomes very big for big dirs
+    # BAD: flat ignores 'filter' for nested dirs
+    def _filter(self, lst, thisdir):
+        # filter /^rel_dir/ on lst
+        # get leftmost path elements
+        # make regex '^' + '|'.join(re.escape(nm)) + '$'
+        thisdir.temporary_filter = re.compile(file_with_matches)
+        thisdir.refilter()
+
+        for f in thisdir.files_all:
+            if f.is_directory:
+                # DEV: each time filter-out one level of files from lst
+                self._filter(lst, f)
+
+    def execute(self):
+        cmd, flags = self._choose()
+        # self.fm.notify(cmd)
+        # TODO:ENH: cmd may be [..] -- no need to shell_escape
+        if self.arg(1) != '-r':
+            self.fm.execute_command(cmd, flags=flags)
+        else:
+            self._filter(self._catch(cmd))
+
+    def tab(self):
+        # BAD:(:ag <prev_patt>) when input alias ':agv' and then <Tab>
+        #   <= EXPL: aliases expanded before parsing cmdline
+        cmd = self.arg(0)
+        flg = self.arg(1)
+        if flg[0] == '-' and flg[1] in 'flvgprw':
+            cmd += ' ' + flg
+        return ['{} {}'.format(cmd, p) for p in reversed(ag.patterns)]
 
 class fd_search(Command):
     """:fd_search [-d<depth>] <query>
@@ -154,3 +273,28 @@ class exif_filter(Command):
         #         self.fm.thisdir.refilter()
         #     i+=1
         # self.fm.thisdir.refilter()
+
+class fzf_rga_documents_search(Command):
+    """
+    :fzf_rga_search_documents
+    Search in PDFs, E-Books and Office documents in current directory.
+    Allowed extensions: .epub, .odt, .docx, .fb2, .ipynb, .pdf.
+
+    Usage: fzf_rga_search_documents <search string>
+    """
+    def execute(self):
+        if self.arg(1):
+            search_string = self.rest(1)
+        else:
+            self.fm.notify("Usage: fzf_rga_search_documents <search string>", bad=True)
+            return
+
+        import subprocess
+        import os.path
+        from ranger.container.file import File
+        command="rga '%s' . --rga-adapters=pandoc,poppler | fzf +m | awk -F':' '{print $1}'" % search_string
+        fzf = self.fm.execute_command(command, universal_newlines=True, stdout=subprocess.PIPE)
+        stdout, stderr = fzf.communicate()
+        if fzf.returncode == 0:
+            fzf_file = os.path.abspath(stdout.rstrip('\n'))
+            self.fm.execute_file(File(fzf_file))
