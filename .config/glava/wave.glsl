6a21d261520ec306b18da73256714d3852766528
Marcelo <mvrozanti@hotmail.com>
2019-12-18 15:11:05 -0300

    cfi≃GLava

--- .config/glava/wave.glsl
+++ .config/glava/wave.glsl
@@ -2,13 +2,13 @@
 #define MIN_THICKNESS 1
 
 /* max (vertical) line thickness */
-#define MAX_THICKNESS 6
+#define MAX_THICKNESS 3
 
 /* base color to use, distance from center will multiply the RGB components */
-#define BASE_COLOR vec4(0.7, 0.2, 0.45, 1)
+#define BASE_COLOR vec4(1.0, 1.0, 0.0, 1)
 
 /* amplitude */
-#define AMPLIFY 500
+#define AMPLIFY 2000
 
 /* outline color */
-#define OUTLINE vec4(0.15, 0.15, 0.15, 1)
+#define OUTLINE vec4(0.0, 0.0, 0.0, 0.1)
