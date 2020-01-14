84532e2f59e99898bfbc14b06b14474e31f0affd
Marcelo <mvrozanti@hotmail.com>
2020-01-14 10:18:26 -0300

    cfcf /home/nexor/.local/bin/mbsync-notify.sh

---
+++ .local/bin/mbsync-notify.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sync_output=`mbsync -V hotmail`
+slave_count=`grep 'Inbox' -A6 <<< $sync_output | grep -E '^slave' | cut -d',' -f1 | tr -cd '[[:digit:]]'`
+master_count=`grep 'Inbox' -A6 <<< $sync_output | grep -E '^master' | cut -d',' -f1 | tr -cd '[[:digit:]]'`
+new_mail_count=$(($master_count - $slave_count))
+echo $new_mail_count
+if [[ $new_mail_count -gt 2 ]]; then
+  notify-send "
+  You have $new_mail_count new emails."
+elif [[ $new_mail_count -eq 1 ]]; then
+  notify-send "
+  You have $new_mail_count new email."
+fi
+
