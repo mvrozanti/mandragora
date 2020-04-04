ff127e89d09ca85481b70e2528952e54476a06d5
Marcelo <mvrozanti@hotmail.com>
2020-04-04 17:47:14 -0300

    v /home/nexor/.local/bin/mbsync-notify.sh

--- .local/bin/mbsync-notify.sh
+++ .local/bin/mbsync-notify.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 sync_output=`mbsync -V hotmail`
+ps aux | grep /usr/local/bin/dota && exit 0
 slave_count=`grep 'Inbox' -A6 <<< $sync_output | grep -E '^slave' | cut -d',' -f1 | tr -cd '[[:digit:]]'`
 master_count=`grep 'Inbox' -A6 <<< $sync_output | grep -E '^master' | cut -d',' -f1 | tr -cd '[[:digit:]]'`
 new_mail_count=$(($master_count - $slave_count))
