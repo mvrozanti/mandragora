61869a03d89d7a8f09d41bd213e5a27fd5b688bb
Marcelo <mvrozanti@hotmail.com>
2024-06-08 22:00:20 -0300

    ≃ptpython

--- .ptpython/ptpythonrc.py
+++ .ptpython/ptpythonrc.py
@@ -1,6 +1,16 @@
+# ptpython-exclusive
+
+__all__ = ["configure"]
+
+def configure(repl):
+    repl.confirm_exit = True
+
+# extra
+
 import os
 import sys
 from os import path as op
 d = dir
 for a in sys.argv:
     os.system(f'ptpython --vi -i {a}')
+
