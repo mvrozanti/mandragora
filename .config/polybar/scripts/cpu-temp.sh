68c974470250ed3c0921bc752aad4b72181c9f78
Marcelo <mvrozanti@hotmail.com>
2025-08-24 14:27:29 -0300

    cfP,cfcf /home/m/.config/polybar/scripts/cpu-temp.sh

---
+++ .config/polybar/scripts/cpu-temp.sh
@@ -0,0 +1,10 @@
+sensors | awk '/^Package id 0:/ {
+    gsub("\\+","",$4); gsub("°C","",$4);
+    temp=$4+0;
+    if (temp < 40)  icon="";
+    else if (temp < 55) icon="";
+    else if (temp < 65) icon="";
+    else if (temp < 75) icon="";
+    else icon="";
+    print icon " " temp "°C";
+}'
