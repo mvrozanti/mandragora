6ac980a96598d5af000ab127757de67d391efc90
Marcelo <mvrozanti@hotmail.com>
2025-10-19 13:12:11 -0300

    aa dush. du -sh .

--- .config/polybar/scripts/updates-pacman.sh
+++ .config/polybar/scripts/updates-pacman.sh
@@ -4,9 +4,9 @@ if ! updates=$(checkupdates 2> /dev/null | wc -l ); then
     updates=0
 fi
 
-if [ "$updates" -gt 20 ]; then
+if [ "$updates" -gt 600 ]; then
     echo "%{u#f00} $updates "
-elif [ "$updates" -gt 10 ]; then
+elif [ "$updates" -gt 400 ]; then
     echo "%{u#ff0} $updates "
 elif [ "$updates" -gt 0 ]; then
     echo " $updates "
