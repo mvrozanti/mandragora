df90185eca1f449b9e0fbc974265df8aea8d0b0f
mvrozanti <mvrozanti@hotmail.com>
2026-04-26 15:23:22 -0300

    feat(networking): add oracle host resolution via dnsmasq and sops secret

--- modules/core/security.nix
+++ modules/core/security.nix
@@ -16,9 +16,9 @@
     enable = true;
     settings = {
       Resolve = {
-        DNS = "1.1.1.1#cloudflare-dns.com 1.0.0.1#cloudflare-dns.com 2606:4700:4700::1111#cloudflare-dns.com";
+        DNS = "127.0.0.1 1.1.1.1#cloudflare-dns.com 1.0.0.1#cloudflare-dns.com 2606:4700:4700::1111#cloudflare-dns.com";
         FallbackDNS = "9.9.9.9#dns.quad9.net 149.112.112.112#dns.quad9.net";
-        DNSOverTLS = "yes";
+        DNSOverTLS = "opportunistic";
         DNSSEC = "allow-downgrade";
         LLMNR = "false";
         Domains = "~.";
