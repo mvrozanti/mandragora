a82cdae9892cb2bc26a46037a5ae9d75a1426a4d
mvrozanti <mvrozanti@hotmail.com>
2026-04-26 09:34:36 -0300

    impermanence: persist /var/lib/private at 0700 to fix ollama DynamicUser

--- modules/core/impermanence.nix
+++ modules/core/impermanence.nix
@@ -5,8 +5,10 @@
     "d /persistent/etc 0755 root root - -"
     "d /persistent/home 0755 root root - -"
     "d /persistent/home/m 0750 m users - -"
-  ] ++ lib.optional config.services.ollama.enable
-    "d /persistent/var/lib/private/ollama 0700 ollama ollama - -";
+  ] ++ lib.optionals config.services.ollama.enable [
+    "d /persistent/var/lib/private 0700 root root - -"
+    "d /persistent/var/lib/private/ollama 0700 ollama ollama - -"
+  ];
 
   environment.etc."machine-id".source = "/persistent/etc/machine-id";
   environment.etc."nixos/mandragora".source = "/persistent/mandragora";
@@ -20,7 +22,10 @@
       "/var/lib/systemd/timers"
       "/etc/NetworkManager/system-connections"
       { directory = "/home/m"; user = "m"; group = "users"; mode = "0750"; }
-    ] ++ lib.optional config.services.ollama.enable "/var/lib/private/ollama"
+    ] ++ lib.optionals config.services.ollama.enable [
+        { directory = "/var/lib/private"; user = "root"; group = "root"; mode = "0700"; }
+        "/var/lib/private/ollama"
+      ]
       ++ lib.optional config.services.prometheus.enable
         { directory = "/var/lib/prometheus2"; user = "prometheus"; group = "prometheus"; mode = "0700"; };
     files = lib.optionals config.services.openssh.enable [
