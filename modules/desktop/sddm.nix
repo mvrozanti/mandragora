386f4738073fb534eed942d266c5d8487936681f
mvrozanti <mvrozanti@hotmail.com>
2026-04-26 14:39:26 -0300

    feat(sddm): autologin user m into hyprland

--- modules/desktop/sddm.nix
+++ modules/desktop/sddm.nix
@@ -1,15 +1,22 @@
 { config, pkgs, lib, ... }:
 {
-  services.displayManager.sddm = {
-    enable = true;
-    wayland.enable = true;
-    package = pkgs.kdePackages.sddm;
-    theme = "sddm-mandragora";
-    extraPackages = with pkgs.kdePackages; [
-      qtsvg
-      qtmultimedia
-      qtvirtualkeyboard
-    ];
+  services.displayManager = {
+    sddm = {
+      enable = true;
+      wayland.enable = true;
+      package = pkgs.kdePackages.sddm;
+      theme = "sddm-mandragora";
+      extraPackages = with pkgs.kdePackages; [
+        qtsvg
+        qtmultimedia
+        qtvirtualkeyboard
+      ];
+    };
+    autoLogin = {
+      enable = true;
+      user = "m";
+    };
+    defaultSession = "hyprland";
   };
 
   environment.systemPackages = [ pkgs.sddm-mandragora ];
