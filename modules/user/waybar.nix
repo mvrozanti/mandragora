a28edab62d3e7b58e6e2c54644a17bfe9ee7775e
mvrozanti <mvrozanti@hotmail.com>
2026-04-28 23:50:11 -0300

    fix(waybar): screencap exec uses PATH not stale ~/.local/bin path

--- modules/user/waybar.nix
+++ modules/user/waybar.nix
@@ -125,12 +125,12 @@
         };
 
         "custom/screencap" = {
-          exec = "~/.local/bin/screencap status";
+          exec = "screencap status";
           return-type = "json";
           interval = 1;
           signal = 11;
-          on-click = "~/.local/bin/capture toggle";
-          on-click-right = "~/.local/bin/capture stop";
+          on-click = "capture toggle";
+          on-click-right = "capture stop";
         };
 
         "custom/weather" = {
