a5450549fdf832a5c7b56a88af94848051600ee0
mvrozanti <mvrozanti@hotmail.com>
2026-04-29 21:29:51 -0300

    refactor(mandragora-vps): retire openvpn, harden ssh setup, embed primary key

--- hosts/mandragora-vps/default.nix
+++ hosts/mandragora-vps/default.nix
@@ -4,15 +4,12 @@
   imports = [
     ./hardware-configuration.nix
 
-    ../../modules/core/globals.nix
     ../../modules/core/secrets.nix
-    ../../modules/core/security.nix
 
     ../../modules/vps/oci.nix
     ../../modules/vps/tailscale.nix
     ../../modules/vps/ddns.nix
     ../../modules/vps/seafile.nix
-    ../../modules/vps/openvpn.nix
     ../../modules/vps/hummingbot.nix
     ../../modules/vps/crypto-fetcher.nix
     ../../modules/vps/orderbook-collector.nix
@@ -35,9 +32,17 @@
 
   users.users.m = {
     isNormalUser = true;
+    description = "Mandragora Primary User";
     extraGroups = [ "wheel" "docker" ];
-    openssh.authorizedKeys.keys = config.mandragora.globals.sshKeys or [ ];
+    shell = pkgs.zsh;
+    openssh.authorizedKeys.keys = [
+      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDUMYP05r/uKioMR0990Q8ql4fb8pvJz3WjvqJ0ytw5RhvFAPgystANW9YBtNWfRgp8LJWKaoNBH5S0YKPgaY4Kla7AbAEHvEKO8Ci+mPfSRfSWT/Lk8b2FyqU+H7eJ1Iu1NfRgKkOE2SsX0sj0hrOFmR7pjpdzyLiOP7EzrjoMhFVGB5BmSb+gQXFG2E9q7A4CHfEalpx+sxG4DSl1z4o+2LRVMkaZRApjCrf84P85E84/ol/RmMHE4DCEdxLHUyEU3xCFTahW1g4pcPrnXbtr7htpgs3FoxmFFppHPYC5s7z35OrlEmOVjsR642tVS6NlUH4CKTosbJ1+OZv+jYO9 mvrozanti@hotmail.com"
+    ];
   };
+
+  security.sudo.wheelNeedsPassword = false;
+
+  programs.zsh.enable = true;
 
   services.openssh = {
     enable = true;
@@ -52,6 +57,11 @@
     enable = true;
     autoPrune.enable = true;
   };
+
+  environment.systemPackages = with pkgs; [
+    git vim neovim wget curl htop btop tree jq sops age
+    rsync tmux file
+  ];
 
   system.stateVersion = "24.05";
 }
