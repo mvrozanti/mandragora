6ad7cce0c2af9df25b501d16b651b5eca30c6315
mvrozanti <mvrozanti@hotmail.com>
2026-05-01 10:00:37 -0300

    feat(waybar): use fa-server icon for gpu monitor

--- modules/user/waybar.nix
+++ modules/user/waybar.nix
@@ -97,7 +97,7 @@
           interval = 5;
         };
         "custom/gpu" = {
-          format = "󰢯  {}%";
+          format = "  {}%";
           exec = "nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader,nounits";
           interval = 5;
         };
