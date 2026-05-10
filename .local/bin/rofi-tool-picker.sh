54b232f7c5a2db8ec45d2d4b9f4bc1f06aa57c2c
mvrozanti <mvrozanti@hotmail.com>
2026-05-09 21:59:45 -0300

    feat(bin): add gpick and hyprpicker to rofi-tool-picker

--- .local/bin/rofi-tool-picker.sh
+++ .local/bin/rofi-tool-picker.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
-selected=$(printf 'zathura\nPostman\nBruno' | rofi -dmenu -p "Tool")
+selected=$(printf 'zathura\nPostman\nBruno\nGpick\nHyprpicker' | rofi -dmenu -p "Tool")
 case "$selected" in
     zathura)  zathura ;;
     Postman)  postman ;;
     Bruno)    bruno ;;
+    Gpick)    gpick ;;
+    Hyprpicker) hyprpicker -a ;;
 esac
