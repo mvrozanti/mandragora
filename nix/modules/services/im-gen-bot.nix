0e39a34df9d8087c9c6f3210b1880a480a81e2c3
mvrozanti <mvrozanti@hotmail.com>
2026-05-16 15:36:10 -0300

    feat(im-gen-bot): add telegram bot service

---
+++ nix/modules/services/im-gen-bot.nix
@@ -0,0 +1,37 @@
+{ config, lib, pkgs, ... }:
+
+let
+  repo = "/home/m/Projects/im-gen";
+in {
+  systemd.user.services.im-gen-bot = {
+    description = "im-gen Telegram bot (Flux on RTX 5070 Ti)";
+    wantedBy = [ "default.target" ];
+    path = [ pkgs.coreutils pkgs.bash pkgs.gcc ];
+    environment = {
+      IM_GEN_DIR = repo;
+    };
+    serviceConfig = {
+      Type = "simple";
+      WorkingDirectory = repo;
+      ExecStart = "${repo}/bot.sh";
+      Restart = "on-failure";
+      RestartSec = "10s";
+      TimeoutStartSec = "5min";
+      MemoryMax = "22G";
+      MemorySwapMax = "0";
+      OOMScoreAdjust = 1000;
+      OOMPolicy = "kill";
+      TasksMax = 4096;
+      ManagedOOMSwap = "kill";
+      ManagedOOMMemoryPressure = "kill";
+    };
+    unitConfig = {
+      ConditionPathExists = [
+        "/dev/nvidia0"
+        "${repo}/invokeai-venv/bin/python"
+        "${repo}/bot.sh"
+        "${repo}/bot.py"
+      ];
+    };
+  };
+}
