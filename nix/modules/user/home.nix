5c7fe68cc1f636ddc0a8ca4062df37022ed187c4
mvrozanti <mvrozanti@hotmail.com>
2026-05-18 21:19:10 -0300

    fix(kitty): opaque on hyprland side, 10% transparent internally

--- nix/modules/user/home.nix
+++ nix/modules/user/home.nix
@@ -489,7 +489,7 @@ in
       inactive_tab_font_style = "normal";
       foreground = "#dddddd";
       background = "#000";
-      background_opacity = "0.4";
+      background_opacity = "0.9";
       dynamic_background_opacity = "yes";
       dim_opacity = "0.75";
       selection_foreground = "#000000";
