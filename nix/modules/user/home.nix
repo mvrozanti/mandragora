5e4e9e7b4af5e63a7124e4063974e97d7701fa8b
mvrozanti <mvrozanti@hotmail.com>
2026-05-24 17:58:05 -0300

    fix(kitty): set background_opacity to 1.0 for full opacity

--- nix/modules/user/home.nix
+++ nix/modules/user/home.nix
@@ -493,7 +493,7 @@ in
       inactive_tab_font_style = "normal";
       foreground = "#dddddd";
       background = "#000";
-      background_opacity = "0.9";
+      background_opacity = "1.0";
       dynamic_background_opacity = "yes";
       dim_opacity = "0.75";
       selection_foreground = "#000000";
