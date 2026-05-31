b2089c7ee6a2ada1a0f06fe89830e26d38d6b7dd
mvrozanti <mvrozanti@gmail.com>
2026-05-30 23:55:52 -0300

    feat(rofi): add 'Kill window' (hyprctl kill, xkill equivalent) to tool picker

--- .local/bin/rofi-tool-picker.sh
+++ .local/bin/rofi-tool-picker.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
-selected=$(printf 'zathura\nPostman\nBruno\nGpick\nHyprpicker' | rofi -dmenu -p "Tool")
+selected=$(printf 'zathura\nPostman\nBruno\nGpick\nHyprpicker\nKill window' | rofi -dmenu -p "Tool")
 case "$selected" in
     zathura)  zathura ;;
     Postman)  postman ;;
     Bruno)    bruno ;;
     Gpick)    gpick ;;
     Hyprpicker) hyprpicker -a ;;
+    "Kill window") hyprctl kill ;;
 esac
